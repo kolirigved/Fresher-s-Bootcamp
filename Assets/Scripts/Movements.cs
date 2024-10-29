160674aea233f83b32da1eb17e56ebd5cababadf
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-10-29 12:43:02 +0530

    changed movement

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Movements : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Rigidbody2D rb;
+    private float getAxisX;
     public float jumpForce;
     public bool onGround; //To check if player is on ground
-    Rigidbody2D rb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,19 +29,34 @@ public class Movements : MonoBehaviour
     }
     void Update()//Using old input system
     {
-        if(Input.GetKey("right")||Input.GetKey("d"))
+        Move();
+        if (Input.GetKey("right") || Input.GetKey("d"))
         {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            rb.velocity = new Vector2(speed, 0)+rb.velocity;           
+            
         }
-        else if(Input.GetKey("left")||Input.GetKey("a"))
+        else if (Input.GetKey("left") || Input.GetKey("a"))
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            rb.velocity = new Vector2(-speed, 0)+rb.velocity;
+            
         }
-        if(onGround && (Input.GetKeyDown("up")||Input.GetKeyDown("w")))
+        
+        if((Input.GetKeyDown("up")||Input.GetKeyDown("w")))
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            Jump();
+        }
+    }
+    void Move()
+    {
+        getAxisX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveSpeed * getAxisX, rb.velocity.y);
+        
+    }
+    void Jump()
+    {
+        if (onGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
     }
 }
