206f84aea2f243e3ab82abeeaec1f8b3c37d8431
Ishan Kamboj <ISHANKAMBOJ35@GMAIL.COM>
2024-10-29 12:53:31 +0530

    changed movement more

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Movements : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private Rigidbody2D rb;
-    private float getAxisX;
+    public float moveSpeed;
     public float jumpForce;
     public bool onGround; //To check if player is on ground
+
+    private Rigidbody2D rb;
+    private float getAxisX;
+    private float getAxisY;
+     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,36 +30,30 @@ public class Movements : MonoBehaviour
             onGround=false;
         }
     }
-    void Update()//Using old input system
+    void Update()
     {
         Move();
-        if (Input.GetKey("right") || Input.GetKey("d"))
-        {
-            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            
-        }
-        else if (Input.GetKey("left") || Input.GetKey("a"))
-        {
-            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            
-        }
-        
-        if((Input.GetKeyDown("up")||Input.GetKeyDown("w")))
-        {
-            Jump();
-        }
+        Jump();
     }
     void Move()
     {
         getAxisX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveSpeed * getAxisX, rb.velocity.y);
-        
+        if(getAxisX > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (getAxisX < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
     }
     void Jump()
     {
-        if (onGround)
+        getAxisY = Input.GetAxisRaw("Vertical");
+        if (onGround && getAxisY > 0)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce * getAxisY);
         }
     }
 }
